c005d362937734929250c4e45363e870b195f951
Beto <alberto.corona@utsh.edu.mx>
2023-07-13 19:08:35 -0600

    version-1, Nombre

---
+++ app/src/main/java/com/example/agendaandroid/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.agendaandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Content()
+
+        }
+    }
+}
+@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Content(){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Gray)
+            .padding(20.dp)
+            ) {
+        Text("Registros UTSH",
+            fontSize = 35.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        )
+        TextField(value = "", onValueChange = {
+
+        },
+        label = {
+            Text(text = "Nombre")
+        })
+
+
+    }
+}
